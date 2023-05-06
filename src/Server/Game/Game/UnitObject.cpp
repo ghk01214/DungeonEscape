51d10de93c53cd3f93ce4e74b079922fbced9c4d
aida <ghk01214@naver.com>
2023-05-07 04:13:07 +0900

    Player->UnitObject

---
+++ src/Server/Game/Game/UnitObject.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include "UnitObject.h"
+#include "CustomController.h"
+#include "MessageHandler.h"
+#include "TimeManager.h"
+#include "RigidBody.h"
+#include "Transform.h"
+
+UnitObject::UnitObject(int32_t playerID, const Vec3& position, const Quat& rotation, const Vec3& scale) :
+	GameObject{ position, rotation, scale },
+	m_playerID{ playerID },
+	m_aniIndex{ 0 },
+	m_aniFrame{ 0.f },
+	m_scriptType{ server::SCRIPT_TYPE::NONE }
+{
+}
+
+UnitObject::~UnitObject()
+{
+}
+
+void UnitObject::Init()
+{
+	m_controller = AddComponent<CustomController>(L"CustomController");
+	m_scriptType = server::SCRIPT_TYPE::MISTIC;
+}
+
+void UnitObject::Update(double timeDelta)
+{
+	m_controller->Move();
+
+	Vec3 trans{ m_transform->GetPosition() };
+
+	if (m_startSendTransform == true and TimeManager::GetInstance()->Is1FrameIn60F() == true)
+	{
+		//if (trans.x != 0.f and trans.z != 0.f)
+			std::cout << trans.x << ", " << trans.y << ", " << trans.z << "\n";
+		game::Message msg{ m_playerID, ProtocolID::MY_TRANSFORM_ACK };
+		//game::MessageHandler::GetInstance()->InsertSendMessage(msg);
+	}
+}
+
+void UnitObject::LateUpdate(double timeDelta)
+{
+	m_controller->ClearControllerCollisionInfo();
+}
+
+void UnitObject::Release()
+{
+	m_controller = nullptr;
+	GameObject::Release();
+}
+
+void UnitObject::SetAniIndex(int32_t aniIndex)
+{
+	m_aniIndex = aniIndex;
+}
+
+void UnitObject::SetAniFrame(float aniFrame)
+{
+	m_aniFrame = aniFrame;
+}
+
+void UnitObject::SetScriptType(server::SCRIPT_TYPE scriptType)
+{
+	m_scriptType = scriptType;
+}
---
+++ src/Server/Game/Game/UnitObject.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "GameObject.h"
+
+class CustomController;
+
+class UnitObject : public GameObject
+{
+public:
+	UnitObject(int32_t playerID = -1, const Vec3& position = Vec3(), const Quat& rotation = Quat(), const Vec3& scale = Vec3(1.f, 1.f, 1.f));
+	~UnitObject() override;
+
+public:
+	virtual void Init();
+	virtual void Update(double timeDelta) override;
+	virtual void LateUpdate(double timeDelta) override;
+	virtual void Release();
+
+public:
+	constexpr int32_t GetPlayerID() const { return m_playerID; }
+	constexpr int32_t GetAniIndex() const { return m_aniIndex; }
+	constexpr float GetAniFrame() const { return m_aniFrame; }
+	constexpr server::SCRIPT_TYPE GetScriptType() const { return m_scriptType; }
+
+	void SetAniIndex(int32_t aniIndex);
+	void SetAniFrame(float aniFrame);
+	void SetScriptType(server::SCRIPT_TYPE scriptType);
+
+public:
+	CustomController* m_controller = nullptr;
+
+	int32_t m_playerID = 4294967295U;
+
+	int32_t m_aniIndex;
+	float m_aniFrame;
+	server::SCRIPT_TYPE m_scriptType;
+};
