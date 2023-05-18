a214bead6a391c63d7cc173bea24bfed6c3bce09
Song Yong Seob <yongsoub9809@gmail.com>
2023-05-19 00:56:04 +0900

    서버 설정으로 복귀

--- src/Server/Game/Game/CustomController.cpp
+++ src/Server/Game/Game/CustomController.cpp
@@ -230,7 +230,7 @@ Collider* CustomController::GetColliderBelow()
 
 void CustomController::DirectionInput_Player()
 {
-	bool serverConnected = false;
+	bool serverConnected = true;
 
 	if (serverConnected)
 	{
@@ -308,8 +308,8 @@ void CustomController::Movement_Player()
 	CheckOnGround(CollisionInfoType::Enter, surfaceNormal);
 	CheckOnGround(CollisionInfoType::Stay, surfaceNormal);
 
-	if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && m_onGround)
-	//if (m_keyboardSpace && m_onGround)
+	//if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && m_onGround)
+	if (m_keyboardSpace && m_onGround)
 	{
 		PxVec3 up{ 0.f, 1.f, 0.f };
 		m_body->GetPosition() += up * 0.05f;
