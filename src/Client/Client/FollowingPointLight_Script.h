7ff6b11d6ab7bf299ce64c86a375f9a914053f47
ChinoDaiski <callmeson1@naver.com>
2023-09-19 12:28:17 +0900

    점 광원 스크립트 추가, 수정 중

---
+++ src/Client/Client/FollowingPointLight_Script.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "MonoBehaviour.h"
+
+class FollowingPointLight_Script : public MonoBehaviour
+{
+public:
+	FollowingPointLight_Script();
+	~FollowingPointLight_Script();
+
+public:
+	virtual void Start() override;
+	virtual void Update() override;
+	virtual void LateUpdate() override;
+
+};
